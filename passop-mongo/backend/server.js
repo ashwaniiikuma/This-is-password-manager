aaf87deb27c62e12377ed860adda48008ed342b7
ashwaniiikuma <Ashwaniiikumar1122@gmail.com>
2026-02-23 07:31:46 +0530

    second commit

---
+++ passop-mongo/backend/server.js
@@ -0,0 +1,51 @@
+// const express = require("express");
+import express from "express"
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+
+dotenv.config();
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "passop";
+const app = express();
+const port = 3000;
+app.use(bodyParser.json())
+app.use(cors())
+
+await client.connect();
+
+//Get all the passwords
+app.get("/", async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection("password");
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult);
+});
+
+//save a password
+app.post("/", async (req, res) => {
+  const password = req.body
+  const db = client.db(dbName);
+  const collection = db.collection("password");
+  const findResult = await collection.insertOne(password);
+  res.send({success: true, result: findResult})
+});
+
+//Delete the password by id
+app.delete("/", async (req, res) => {
+  const password = req.body
+  const db = client.db(dbName);
+  const collection = db.collection("password");
+  const findResult = await collection.deleteOne(password);
+  res.send({success: true, result: findResult})
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
